b03a07ccb16be449120d1a0f040ed5664738efb4
sankalp srivatava <Sankalp2015415@akgec.ac.in>
2022-10-24 11:30:23 +0530

    captionDay2

--- lib/views/screens/caption_screen.dart
+++ lib/views/screens/caption_screen.dart
@@ -46,117 +46,164 @@ class _CaptionScreenState extends State<CaptionScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 300,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Caption",
-                hintText: "Write your Caption here",
-                fillColor: Colors.transparent,
-                filled: true,
-                contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: const BorderSide(color: Colors.grey)),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(color: Colors.grey.shade400)),
-              ),
-              keyboardType: TextInputType.multiline,
-              minLines: 1, //Normal textInputField will be displayed
-              maxLines: 100,
-              // decoration: ThemeHelper().textInputDecoration(
-              //     'Applicant Name', 'Enter Applicant Name'),
-              validator: (val) {
-                return null;
-              },
-            ),
-            // Container(
-            //   decoration: BoxDecoration(
-            //     borderRadius: BorderRadius.circular(10.0),
-            //     color: Colors.green,
-            //   ),
-            //   width: MediaQuery.of(context).size.width / 1,
-            //   height: MediaQuery.of(context).size.height / 1.2,
-            //   //child: VideoPlayer(controller),
-            // ),
-            const SizedBox(
-              height: 30,
-            ),
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
             SingleChildScrollView(
-              scrollDirection: Axis.vertical,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  // Container(
-                  //   margin: const EdgeInsets.symmetric(horizontal: 10),
-                  //   width: MediaQuery.of(context).size.width - 20,
-                  //   child: TextInputField(
-                  //     controller: _songController,
-                  //     labelText: 'Song Name',
-                  //     icon: Icons.music_note,
-                  //   ),
-                  // ),
-                  // const SizedBox(
-                  //   height: 10,
-                  // ),
-                  // Container(
-                  //   margin: const EdgeInsets.symmetric(horizontal: 10),
-                  //   width: MediaQuery.of(context).size.width - 20,
-                  //   child: TextInputField(
-                  //     controller: _captionController,
-                  //     labelText: 'Caption',
-                  //     icon: Icons.closed_caption,
-                  //   ),
-                  // ),
-                  const SizedBox(
-                    height: 10,
-                  ),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
+                      const SizedBox(
+                        width: 5,
+                      ),
                       InkWell(
-                        onTap: () => {},
+                        onTap: () => {Navigator.pop(context)},
                         child: Container(
-                          width: MediaQuery.of(context).size.width * 0.3,
+                          width: MediaQuery.of(context).size.width * 0.11,
                           height: MediaQuery.of(context).size.width * 0.11,
                           decoration: BoxDecoration(
-                            color: Colors.blue,
+                            color: Colors.transparent,
                             borderRadius: BorderRadius.all(Radius.circular(
-                                MediaQuery.of(context).size.width * 0.015)),
+                                MediaQuery.of(context).size.width * 0.11 / 2)),
                           ),
-                          child: Center(
-                              child: Text(
-                            "Upload",
-                            style: TextStyle(
-                                fontSize:
-                                    MediaQuery.of(context).size.width * 0.06),
-                          )),
-                          //Icon(Icons.arrow_forward_ios_rounded),
+                          child: Icon(Icons.arrow_back_ios_new_rounded),
                         ),
                       ),
-                      // ElevatedButton(
-                      //     onPressed: () =>
-                      //         {}, //uploadVideoController.uploadVideo(
-                      //     // _songController.text,
-                      //     // _captionController.text,
-                      //     // widget.videoPath),
-                      //     child: const Text(
-                      //       'Next',
-                      //     )),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.045,
+                      Container(
+                        child: Text(
+                          "Add caption",
+                          style: TextStyle(fontSize: 20),
+                        ),
                       )
                     ],
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * 0.045,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25.0),
+                      color: Colors.green,
+                    ),
+                    width: MediaQuery.of(context).size.width / 2.5,
+                    height: MediaQuery.of(context).size.height / 2.95,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * 0.05,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Caption",
+                      hintText: "Write your Caption here",
+                      fillColor: Colors.transparent,
+                      filled: true,
+                      contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: const BorderSide(color: Colors.grey)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(color: Colors.grey.shade400)),
+                    ),
+                    keyboardType: TextInputType.multiline,
+                    minLines: 1, //Normal textInputField will be displayed
+                    maxLines: 100,
+                    // decoration: ThemeHelper().textInputDecoration(
+                    //     'Applicant Name', 'Enter Applicant Name'),
+                    validator: (val) {
+                      return null;
+                    },
+                  ),
+                  // Container(
+                  //   decoration: BoxDecoration(
+                  //     borderRadius: BorderRadius.circular(10.0),
+                  //     color: Colors.green,
+                  //   ),
+                  //   width: MediaQuery.of(context).size.width / 1,
+                  //   height: MediaQuery.of(context).size.height / 1.2,
+                  //   //child: VideoPlayer(controller),
+                  // ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // Container(
+                        //   margin: const EdgeInsets.symmetric(horizontal: 10),
+                        //   width: MediaQuery.of(context).size.width - 20,
+                        //   child: TextInputField(
+                        //     controller: _songController,
+                        //     labelText: 'Song Name',
+                        //     icon: Icons.music_note,
+                        //   ),
+                        // ),
+                        // const SizedBox(
+                        //   height: 10,
+                        // ),
+                        // Container(
+                        //   margin: const EdgeInsets.symmetric(horizontal: 10),
+                        //   width: MediaQuery.of(context).size.width - 20,
+                        //   child: TextInputField(
+                        //     controller: _captionController,
+                        //     labelText: 'Caption',
+                        //     icon: Icons.closed_caption,
+                        //   ),
+                        // ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            InkWell(
+                              onTap: () => {},
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.3,
+                                height:
+                                    MediaQuery.of(context).size.width * 0.11,
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(
+                                          MediaQuery.of(context).size.width *
+                                              0.015)),
+                                ),
+                                child: Center(
+                                    child: Text(
+                                  "Upload",
+                                  style: TextStyle(
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.06),
+                                )),
+                                //Icon(Icons.arrow_forward_ios_rounded),
+                              ),
+                            ),
+                            // ElevatedButton(
+                            //     onPressed: () =>
+                            //         {}, //uploadVideoController.uploadVideo(
+                            //     // _songController.text,
+                            //     // _captionController.text,
+                            //     // widget.videoPath),
+                            //     child: const Text(
+                            //       'Next',
+                            //     )),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.045,
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
